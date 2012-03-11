b27f4c65d35a7f920937fb6d072cf6954e528d82
Sarim Khan <sarim2005@gmail.com>
2012-03-11 14:25:09 +0600

    bang travis again

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mkdir dist
 zip dist/banglish2bangla.crx -r9 . -x build.sh  .git  .git/\*  .travis.yml
-git commit -a -m "Build on `DATE`"
+git commit -a -m "Build on `DATE` [ci skip]"
 git push
